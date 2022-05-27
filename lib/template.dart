694b831aadcf714f2dff77c48ee15f16c4bedd57
Apridev <apriantofajar11@gmail.com>
2022-05-27 13:59:54 +0900

    template app

---
+++ lib/template.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color defaultColor = Color(0xff232931);
+Color primaryColor = Color(0xff483434);
+Color subPrimaryColor = Color(0xff6B4F4F);
+Color subPrimaryColor2 = Color(0xffEED6C4);
+Color subPrimaryColor3 = Color(0xffFFF3E4);
+Color backgroundColor = Color(0xffffffff);
+Color backgroundColor2 = Color(0xffEDEDED);
+Color backgroundColor3 = Color(0xffD1D1D1);
+Color backgroundColor4 = Color(0xffFAFAFA);
+Color searchColor = Color(0xffA5A5A5);
+Color subBackgroundColor = Color(0xffB2B1B9);
+Color starColor = Color(0xffFFC700);
+
+double defaultMargin = 20.0;
+
+TextStyle defaultTextStyle = GoogleFonts.openSans(
+  color: defaultColor
+);
+
+TextStyle primaryTextStyle = GoogleFonts.openSans(
+  color: primaryColor
+);
+
+TextStyle subPrimaryTextStyle = GoogleFonts.openSans(
+  color: subPrimaryColor
+);
+
+TextStyle subPrimaryTextStyle2 = GoogleFonts.openSans(
+  color: subPrimaryColor2
+);
+
+TextStyle subPrimaryTextStyle3 = GoogleFonts.openSans(
+  color: subPrimaryColor3
+);
+
+TextStyle backgroundTextStyle = GoogleFonts.openSans(
+  color: backgroundColor
+);
+
+TextStyle backgroundTextStyle2 = GoogleFonts.openSans(
+  color: backgroundColor2
+);
+
+TextStyle backgroundTextStyle3 = GoogleFonts.openSans(
+  color: backgroundColor3
+);
+
+TextStyle subBackgroundTextStyle = GoogleFonts.openSans(
+  color: subBackgroundColor
+);
+
+TextStyle searchTextStyle = GoogleFonts.openSans(
+  color: searchColor
+);
+
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
