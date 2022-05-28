a50cd690df9188ce04859758bef49858f2136b0d
Apridev <apriantofajar11@gmail.com>
2022-05-28 13:03:42 +0900

    wishlist page

---
+++ lib/home/wishlist_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:public_skincare_app/template.dart';
+
+class WishlistPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Wishlist Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
